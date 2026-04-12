fixed broadcast error

src/kldm/diffusionModels/trivialized_diffusion.py
@@ -81,12 +81,12 @@ class TrivialisedDiffusion(nn.Module):
 
     def _sigma_norm(self, sigma: torch.Tensor, num_samples: int = 20000) -> torch.Tensor:
         sigmas = sigma[None, :].repeat(num_samples, 1)
-        x_sample = sigma * torch.randn_like(sigmas)
+        x_sample = sigmas * torch.randn_like(sigmas)
         x_sample = self.wrap_displacements(x_sample)
         normal = d_log_wrapped_normal(
             r=x_sample,
             mu=torch.zeros_like(x_sample),
-            sigma=sigma,
+            sigma=sigmas,
             K=self.k_wn_score,
         )
         return normal.square().mean(dim=0)