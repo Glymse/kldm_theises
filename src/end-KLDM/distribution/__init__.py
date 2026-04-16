e0165c61221e98cb8716e7fb73a4dd4ceca5b9b0
S234845 <S234845@dtu.dk>
2026-04-16 16:24:36 +0000

    ss

--- src/end-KLDM/distribution/__init__.py
+++ src/end-KLDM/distribution/__init__.py
@@ -1,4 +1,4 @@
 from .uniform import sample_uniform
-from .wrapped_normal import d_log_wrapped_normal
+from .wrapped_normal import d_log_wrapped_normal, sigma_norm
 
-__all__ = ["sample_uniform", "d_log_wrapped_normal"]
+__all__ = ["sample_uniform", "d_log_wrapped_normal", "sigma_norm"]
